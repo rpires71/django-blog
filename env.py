be0d8b4c09cf12b905e64ed076a145a91e5d9ae2
Roberto Pires <roberto.pires@gmail.com>
2025-12-19 10:16:08 +0000

    Databses and Deployment

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault('DATABASE_URL', 'postgresql://neondb_owner:npg_DQK6Edc5PrJt@ep-royal-haze-ag06p8pj.c-2.eu-central-1.aws.neon.tech/going_elves_crave_815039')  
+
+os.environ.setdefault('SECRET_KEY', 'L.Z1e1p1p1DB')
